7e650d45f64fc80c34002cd6135db8b36616f2ab
rezi-0415 <3410021977>
2019-11-24 20:12:06 +0800

    work10

--- work7slide/index.js
+++ work7slide/index.js
@@ -1,12 +1,17 @@
-function sort(methob){
-    let origin = document.getElementById('origin').Value
+function sort(method){
+    //读取排序数据
+    let origin = document.getElementById('origin').value
     let logs
-    console.log(methob)
-    if(methob=='bubble'){
+    console.log(method)
+    //根据传入排序的方法调用不同的排序函数
+    if(method=='bubble'){
         logs = bubbleSort(origin)
+    }else if(method=='insert'){
+        logs = insertSort(origin)
     }
     showDiv = document.getElementById('Anim')
-    logValues = logs.Values(
+    logValues = logs.values()
     console.log(logValues.next())
     setTimeout("showLog(logValues,showDiv)",50)
+    showDiv.innerHTML = logs
 }
